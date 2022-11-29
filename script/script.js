c55b04fbbdb99e025df33ca234c3c3ca482971f1
Roman <108659195+romariozt@users.noreply.github.com>
2022-11-29 18:24:58 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -4,8 +4,15 @@ $('.hiring__prof > div').on('click', function () {
     $(this).next('.hiring__prof-text').slideToggle(300);
 })
 
+
 $('.features__item-box').on('click', function () {
     $(this).toggleClass('change__color-title');
     $(this).parent('.features__box').toggleClass('features__box-border');
     $(this).next('.features__item-text').slideToggle(300);
+})
+
+
+$('#hiring-link').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $('#hiring').offset().top}, 1000)
 })
